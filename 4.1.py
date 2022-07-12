36f1a3565910005aa8e82696cca42a048f69e758
tol <krooked222@mail.ru>
2022-07-12 13:55:22 +0300

    commit

---
+++ 4.1.py
@@ -0,0 +1,12 @@
+#4.1
+pizzez = ['Салями','Фирменная','Курица и грибы']
+for pizza in pizzez:
+    print(pizza)
+#4.2
+for pizza in pizzez:
+    print("I like " + pizza + " pizza")
+#4.3
+for pizza in pizzez:
+    print("I like " + pizza + " pizza")
+print('I realyy love pizza')
+
