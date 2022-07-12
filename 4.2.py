91dbd10de2e3d2937ef96b8e8cc8602e92d2bf20
tol <krooked222@mail.ru>
2022-07-12 14:01:45 +0300

    commit

---
+++ 4.2.py
@@ -0,0 +1,8 @@
+#4.2
+pets = ["cat", "dog", "bird"]
+for pet in pets:
+    print(pet)
+#4.2(1)
+for pet in pets:
+    print("A " + pet + " would make a great pet.")
+print("Any of these animals would make a great pet!")
