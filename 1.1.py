3ffa2fd0e46910a777e3d61f9edf16c364f9216f
tol <krooked222@mail.ru>
2022-07-12 12:57:11 +0300

    commit

---
+++ 1.1.py
@@ -0,0 +1,9 @@
+names = ['john', 'NINA', 'sara', 'brad']
+print(names[0])
+print(names[1].lower())
+print(names[2].upper())
+print(names[3].title())
+print("Hi, " + names[0].title())
+print("Hi, " + names[1].title())
+print("Hi, " + names[2].title())
+print("Hi, " + names[3].title())
