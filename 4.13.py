f64f7f4ad1ca5c1c0966f66b30e88e5e28de4fed
tol <krooked222@mail.ru>
2022-07-13 12:09:02 +0300

    commit

---
+++ 4.13.py
@@ -0,0 +1,7 @@
+kortezh = ('eda1', 'eda2', 'eda3', 'eda4')
+for i in kortezh:
+    print(i)
+print("\nNew kortezh")
+new_kortezh = ('eda1', 'eda7', 'eda9', 'eda4')
+for i in new_kortezh:
+    print(i)
