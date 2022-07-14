8e086441f16c6124f12f1d05a95d54067e50b54f
tol <krooked222@mail.ru>
2022-07-14 22:22:53 +0300

    commit

--- 6.5.py
+++ 6.5.py
@@ -1,7 +1,7 @@
-river = {'nile': 'egypt',
+'''river = {'nile': 'egypt',
          'dnepr': 'ukraine',
          'missisipi': 'USA',
-         }
+         }'''
 '''for key, vault in river.items():
     print('\nThe ' + key.title() + ' runs through ' + vault.title() + '.')
 
@@ -10,3 +10,18 @@ for name in river.keys():
 
 for name in river.values():
     print('\nCity: ' + name.title())'''
+
+'''favorite_languages = {
+'jen': 'python',
+'sarah': 'c',
+'edward': 'ruby',
+'phil': 'python',
+}
+
+names = ['Marina', 'Tolian', 'Serg', 'jen','edward']
+
+for name in names:
+    if name not in favorite_languages.keys():
+        print('\n' + name + ' пройдите опрос')
+    else:
+        print('\nСпасибо ' + name + ' вы уже прошли опрос')'''
