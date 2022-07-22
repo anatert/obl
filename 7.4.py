9e4bdce615549badeca3b4ffa6395c33e116ee38
tol <krooked222@mail.ru>
2022-07-22 09:53:32 +0300

    commit

--- 7.4.py
+++ 7.4.py
@@ -1,5 +1,27 @@
-topping = str(input("Choose topping: "))
+'''topping = str(input("Choose topping: "))
 print('\nprint "quit" to exit order')
 msg = ""
 while topping != "quit":
-    print(message)
+    print(message)'''
+
+
+
+'''message = 'Add topping'
+message += '\n"quit" to exit order\n'
+adding_toping = ''
+while True:
+    adding_toping = input(message)
+    print(f'{adding_toping} added!\n')'''
+
+'''message = 'vozrast: '
+while True:
+    age = (input(message))
+    if int(age) < 3:
+        print('price: free')
+    elif int(age) >= 3 and int(age) < 12:
+        print('price 10$')
+    elif int(age) >= 12:
+        print('price: 12$')'''
+
+
+
